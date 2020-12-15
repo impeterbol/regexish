8bc426408d9f9c9f76021fd654622d82cd2f39aa
peterbolyrev <peterboldyrev@users.noreply.github.com>
2020-12-14 19:12:25 -0800

    adding textarea for convenience

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 console.log("CONNECTED")
+document.querySelectorAll('.btnPfams').onclick = function() {
+    console.log('testing')
+}
+
 
 function adjustPfams(){
     console.log("working, btn pressed")
@@ -7,5 +11,6 @@ function adjustPfams(){
     let numOfPfams = newPfams.split(';').length
     document.getElementById('finalNumOfPfams').innerHTML = numOfPfams
     document.getElementById('finalPfams').innerHTML = newPfams
+    document.getElementById('finalPfams').disabled = true
 }
 
