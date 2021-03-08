8205e30b7400b4254d59bd57cc32d103cfd5887d
peterbolyrev <peterboldyrev@users.noreply.github.com>
2021-03-08 14:58:10 -0800

    added comparison feature

--- script.js
+++ script.js
@@ -7,10 +7,28 @@ document.querySelectorAll('.btnPfams').onclick = function() {
 function adjustPfams(){
     console.log("working, btn pressed")
     let userInput = document.getElementById('pfamsField').value
+    console.log(userInput)
     let newPfams = userInput.replace(/\n/g,";")
     let numOfPfams = newPfams.split(';').length
     document.getElementById('finalNumOfPfams').innerHTML = numOfPfams
     document.getElementById('finalPfams').innerHTML = newPfams
     document.getElementById('finalPfams').disabled = true
 }
+
+function comparePfams(){
+    console.log("working");
+    let userInputExcel = document.getElementById('compareInitialTextArea');
+    let userInputProgramConfig = document.getElementById('compareFromConfigTextArea');
+    let updUserInput = userInputExcel.value.replace(/\n/g,";")
+    
+    console.log(userInputExcel.value)
+    console.log(userInputProgramConfig.value)
+    if (updUserInput == userInputProgramConfig.value)
+    userInputExcel.style.borderColor = "#2EFE2E";
+  else
+  userInputExcel.style.borderColor = "red";
+  
+
+}
+
 
