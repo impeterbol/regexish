9e61e5801fb8b0062ac6e5be6b92bd3892a5296c
peterbolyrev <peterboldyrev@users.noreply.github.com>
2021-07-26 19:48:03 -0700

    adding Dark Mode + upd text

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-console.log("CONNECTED")
-// document.querySelectorAll('.btnPfams').onclick = function() {
-//     console.log('testing')
-// }
+
+
+// dark mode switch
+const toggleSwitch = document.querySelector('.switch label input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+// saving to localstorage future preferences 
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); 
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }    
+}
+
+// checking user preferences for dark / light theme 
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+//functon end
+}
 
 
 function adjustPfams(){
